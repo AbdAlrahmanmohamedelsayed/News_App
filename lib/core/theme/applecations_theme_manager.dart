518b175aa697326164971dd7aa360fc08aa9730d
abdelrahman <104737626+AbdAlrahmanmohamedelsayed@users.noreply.github.com>
2024-08-21 06:11:15 +0300

    add_splash_view

--- lib/core/theme/applecations_theme_manager.dart
+++ lib/core/theme/applecations_theme_manager.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/core/theme/colors_palettes.dart';
 
 class ApplecationsThemeManager {
-  static ThemeData lighttheme = ThemeData();
+  static ThemeData lighttheme = ThemeData(
+    primaryColor: ColorsPalettes.primaryColor,
+  );
 }
