e61844f0f122be4815f096cfbc244b56a2a1a0f5
abdelrahman <104737626+AbdAlrahmanmohamedelsayed@users.noreply.github.com>
2024-08-26 20:14:50 +0300

    add ArticlesModel

---
+++ lib/model/articles_model.dart
@@ -0,0 +1,41 @@
+class ArticlesModel {
+  final String status;
+  final List<Article> articles;
+  ArticlesModel({
+    required this.status,
+    required this.articles,
+  });
+  factory ArticlesModel.fromjson(Map<String, dynamic> json) {
+    return ArticlesModel(
+        status: json['status'],
+        articles: (json['articles'] as List)
+            .map((e) => Article.fromjson(e))
+            .toList());
+  }
+}
+
+class Article {
+  final String title;
+  final String author;
+  final String description;
+  final String url;
+  final String urlToImage;
+  final String publishedAt;
+  Article({
+    required this.author,
+    required this.title,
+    required this.description,
+    required this.url,
+    required this.urlToImage,
+    required this.publishedAt,
+  });
+  factory Article.fromjson(Map<String, dynamic> json) {
+    return Article(
+        author: json['source']['name'],
+        title: json['title'],
+        description: json['description'],
+        url: json['url'],
+        urlToImage: json['urlToImage'],
+        publishedAt: json['publishedAt']);
+  }
+}
